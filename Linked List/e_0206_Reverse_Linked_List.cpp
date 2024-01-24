27c7a1cb8f582b8a9deb5b67df4cba147fe659be
MondayNick <51749165+mondaynick@users.noreply.github.com>
2024-01-24 22:30:50 +0800

    Create e_0206_Reverse_Linked_List.cpp

---
+++ Linked List/e_0206_Reverse_Linked_List.cpp
@@ -0,0 +1,31 @@
+/*
+  給定一鏈結串列，輸出其反轉結果
+
+  利用two pointer
+ */
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if (!head || !head->next) return head;
+        ListNode *prev = head, *curr = head->next;
+        prev->next = NULL;
+        while (curr) {
+            ListNode* temp = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = temp;
+        }
+        return prev;
+    }
+};
