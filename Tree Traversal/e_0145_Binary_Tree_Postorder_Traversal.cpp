a4fc417bd977c8b56b0fa8e73cbb99a89d798822
MondayNick <51749165+mondaynick@users.noreply.github.com>
2024-01-25 00:09:29 +0800

    Create e_0145_Binary_Tree_Postorder_Traversal.cpp

---
+++ Tree Traversal/e_0145_Binary_Tree_Postorder_Traversal.cpp
@@ -0,0 +1,43 @@
+/*
+  給定一樹根，求postorder traversal
+
+  L -> R -> V
+ */
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> res;
+        if (!root) return res;
+
+        stack<TreeNode*> s;
+        s.push(root);
+        while (!s.empty()) {
+            TreeNode* curr = s.top();
+            if (!curr->left && !curr->right) {
+                s.pop();
+                res.push_back(curr->val);
+            }
+            if (curr->right) {
+                s.push(curr->right);
+                curr->right = NULL;
+            }
+            if (curr->left) {
+                s.push(curr->left);
+                curr->left = NULL;
+            }
+        }
+        return res;
+    }
+};
