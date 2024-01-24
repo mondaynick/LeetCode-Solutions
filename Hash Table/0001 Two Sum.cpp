d6d0c685222d27335a555e47bce984ef7a35f34d
MondayNick <51749165+mondaynick@users.noreply.github.com>
2024-01-24 16:13:45 +0800

    Create 0001 Two Sum.cpp

---
+++ Hash Table/0001 Two Sum.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        map <int, int> dict;
+        vector <int> ans;
+        for (int i = 0 ; i < nums.size() ; i++) {
+            if (dict.find(nums[i]) == dict.end()) {
+                dict[target - nums[i]] = i;
+            } else {
+                ans.push_back(dict[nums[i]]);
+                ans.push_back(i);
+                return ans;
+            }
+        }
+        return ans;
+    }
+};
