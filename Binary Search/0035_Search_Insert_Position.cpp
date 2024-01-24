6995052e06046e832fb39531240fd9312c86180e
MondayNick <51749165+mondaynick@users.noreply.github.com>
2024-01-24 21:55:38 +0800

    Create 0035_Search_Insert_Position.cpp

---
+++ Binary Search/0035_Search_Insert_Position.cpp
@@ -0,0 +1,23 @@
+/*
+  給定陣列nums，其數值為由升冪排序，輸出若要插入整數target，應該插入nums的第幾個位置
+
+  利用二分搜尋法，當兩者交錯時回傳low
+*/
+
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        if (target > nums.back()) return nums.size();
+        int low = 0, high = nums.size();
+        while (low <= high) {
+            int mid = (high + low) / 2;
+            if (nums[mid] == target)
+                return mid;
+            else if (nums[mid] < target)
+                low = mid + 1;
+            else
+                high = mid - 1;
+        }
+        return low;
+    }
+};
