ad85fa64108aa3f0fe8763a0f0696722c683ae78
MondayNick <51749165+mondaynick@users.noreply.github.com>
2024-01-24 22:46:19 +0800

    Update e_0100_Same_Tree.cpp

--- Binary Tree/e_0100_Same_Tree.cpp
+++ Binary Tree/e_0100_Same_Tree.cpp
@@ -1,3 +1,26 @@
 /*
+  給定兩個樹根，判斷兩個樹是否相同
 
+  利用遞迴比較
  */
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if (NULL == p && NULL == q) return true;
+        if (NULL != p && NULL != q && p->val == q->val && isSameTree(p->left, q->left) && isSameTree(p->right, q->right))
+            return true;
+        return false;
+    }
+};
