1b7e93dc3d9223801abb71d4c28e6479b0a7e283
MondayNick <51749165+mondaynick@users.noreply.github.com>
2024-02-05 15:43:45 +0800

    Update e_0169_Majority_Element.cpp

--- Mathematics/e_0169_Majority_Element.cpp
+++ Mathematics/e_0169_Majority_Element.cpp
@@ -2,7 +2,7 @@
   給定一系列數字，求該數字數量超過半數之數字
 
   利用摩爾投票算法(Boyer-Moore Majority Vote Algorithm)：
-    1. 令majority = nums[0], count = 1
+    1. 令majority = 任意數, count = 0
     2. 遍歷nums從第一個到最後一個
         若nums[i] = majority
           count++
@@ -16,15 +16,15 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        int majority = nums[0], count = 1;
-        for (int i = 1 ; i < nums.size() ; i++) {
-            if (nums[i] == majority) {
+        int majority = 0, count = 0;
+        for (auto n: nums) {
+            if (n == majority) {
                 count++;
             } else {
                 if (count > 0) {
                     count--;
                 } else {
-                    majority = nums[i];
+                    majority = n;
                 }
             }
         }
