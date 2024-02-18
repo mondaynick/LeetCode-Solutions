dd1909007ac5b82579f9469a91d88ec7750c854f
MondayNick <51749165+mondaynick@users.noreply.github.com>
2024-02-18 22:16:38 +0800

    Create e_0704_Binary_Search.cpp

---
+++ Binary Search/e_0704_Binary_Search.cpp
@@ -0,0 +1,23 @@
+/*
+  給定一陣列及target，求該陣列target所在的索引
+
+  利用二元搜尋法
+ */
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int start = 0, end = nums.size() - 1;
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            if (nums[mid] < target) {
+                start = mid + 1;
+            } else if (nums[mid] > target) {
+                end = mid - 1;
+            } else {
+                return mid;
+            }
+        }
+        return -1;
+    }
+};
