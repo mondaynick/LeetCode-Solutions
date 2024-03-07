f9f779c09f978ce5e809f55a42694e494b83f5fd
MondayNick <51749165+mondaynick@users.noreply.github.com>
2024-03-07 08:43:19 +0800

    Update m_0015_3Sum.cpp

--- Two Pointer/m_0015_3Sum.cpp
+++ Two Pointer/m_0015_3Sum.cpp
@@ -12,25 +12,26 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
         vector<vector<int>> res;
-        for (int i = 0 ; i < nums.size() - 2 ; i++) {
-            if (i == 0 || (i > 0) && (nums[i] != nums[i - 1])) {
-                int j = i + 1;
-                int k = nums.size() - 1;
-                int target = 0 - nums[i];
+        int len = nums.size();
+        sort(nums.begin(), nums.end());
+
+        for (int i = 0 ; i < len - 2 ; i++) {
+            if (i == 0 || nums[i] != nums[i - 1]) {
+                int j = i + 1, k = len - 1;
                 while (j < k) {
-                    if (nums[j] + nums[k] == target) {
+                    int sum = nums[i] + nums[j] + nums[k];
+                    if (sum == 0) {
                         res.push_back({nums[i], nums[j], nums[k]});
-                        while (j < k && nums[j] == nums[j + 1]) j++;
-                        while (j < k && nums[k] == nums[k - 1]) k--;
                         j++, k--;
-                    } else if (nums[j] + nums[k] < target) {
-                        while (j < k && nums[j] == nums[j + 1]) j++;
+                        while (j < k && nums[j] == nums[j - 1]) j++;
+                        while (j < k && nums[k] == nums[k + 1]) k--;
+                    } else if (sum < 0) {
                         j++;
+                        while (j < k && nums[j] == nums[j - 1]) j++;
                     } else {
-                        while (j < k && nums[k] == nums[k - 1]) k--;
                         k--;
+                        while (j < k && nums[k] == nums[k + 1]) k--;
                     }
                 }
             }
