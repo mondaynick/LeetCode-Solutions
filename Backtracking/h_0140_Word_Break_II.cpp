76bae190d878d7843db6ddab18ea4a333c0c6504
MondayNick <51749165+mondaynick@users.noreply.github.com>
2024-03-10 22:31:56 +0800

    Update h_0140_Word_Break_II.cpp

--- Backtracking/h_0140_Word_Break_II.cpp
+++ Backtracking/h_0140_Word_Break_II.cpp
@@ -1,35 +1,46 @@
 /*
   給定一字串s，回傳此字串可以由wordDict中的單字如何組合而成，列出所有可能性
 
-  利用backtracking，
+  wordBreak("catsanddog")
+  -> wordBreak("") + indict("catsanddog")
+  -> wordBreak("c") + indict("atsanddog")
+  -> wordBreak("ca") + indict("tsanddog")
+  -> wordBreak("cat") + indict("sanddog")
+  -> wordBreak("cats") + indict("anddog")
+  -> wordBreak("catsa") + indict("nddog")
+  -> wordBreak("catsan") + indict("ddog")
+  -> wordBreak("catsand") + indict("dog")
+  -> wordBreak("catsandd") + indict("og")
+  -> wordBreak("catsanddo") + indict("g")
  */
 
 class Solution {
 public:
     vector<string> wordBreak(string s, vector<string>& wordDict) {
-        unordered_set<string> dict(wordDict.cbegin(), wordDict.cend());
-        return wordBreak(s, dict);
+        unordered_set<string> dict (wordDict.begin(), wordDict.end());
+        return wordBreak(dict, s);
     }
-private:
-    vector<string> append(const vector<string>& prefixes, const string& word) {
+
+    vector<string> wordBreak(unordered_set<string> dict, string s) {
+        if (mem.find(s) != mem.end()) return mem[s];
         vector<string> res;
-        for (const auto& prefix: prefixes)
-            res.push_back(prefix + " " + word);
+        if (dict.count(s)) res.push_back(s);
+        for (int i = 1 ; i < s.length() ; i++) {
+            string right = s.substr(i);
+            if (!dict.count(right)) continue;
+
+            string left = s.substr(0, i);
+            vector<string> temp = append(wordBreak(dict, left), right);
+            res.insert(res.end(), temp.begin(), temp.end());
+        }
+        return mem[s] = res;
+    }
+
+    vector<string> append(vector<string> prefixes, string s) {
+        vector<string> res;
+        for (auto prefix: prefixes) res.push_back(prefix + " " + s);
         return res;
     }
 
-    const vector<string>& wordBreak(string s, unordered_set<string>& dict) {
-        if (mem.count(s)) return mem[s];
-        vector<string> ans;
-        if (dict.count(s)) ans.push_back(s);
-        for (int i = 1 ; i < s.length() ; i++) {
-            const string& right = s.substr(i);
-            if (!dict.count(right)) continue;
-            const string& left = s.substr(0, i);
-            const vector<string> left_ans = append(wordBreak(left, dict), right);
-            ans.insert(ans.end(), left_ans.begin(), left_ans.end());
-        }
-        return mem[s] = ans;
-    }
     unordered_map<string, vector<string>> mem;
 };
