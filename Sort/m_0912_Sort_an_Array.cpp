289e7229088b65c5ebc6ad7ba81b3c406a6c66f5
MondayNick <51749165+mondaynick@users.noreply.github.com>
2024-04-09 16:26:13 +0800

    Update m_0912_Sort_an_Array.cpp

--- Sort/m_0912_Sort_an_Array.cpp
+++ Sort/m_0912_Sort_an_Array.cpp
@@ -7,18 +7,25 @@
 class Solution {
 public:
     vector<int> sortArray(vector<int>& nums) {
-        function<void(int, int)> quickSort = [&](int start, int end) {
-            if (start >= end) return;
-            int left = start, right = end;
-            int idx = start + (rand() % (end - start + 1));
-            int pivot = nums[idx];
-            while (left <= right) {
-                while (nums[left] < pivot) left++;
-                while (nums[right] > pivot) right--;
-                if (left <= right) swap(nums[left++], nums[right--]);
+        function <void(int, int)> quickSort = [&] (int start, int end) {
+            if (start < end) {
+                /* Randomize the Pivot */
+                int idx = start + rand() % (end - start + 1);
+                swap(nums[idx], nums[start]);
+
+                /* Hoare Partition Scheme */
+                int pivot = nums[start], left = start + 1, right = end;
+                while (left <= right) {
+                    while (left <= right && nums[left] <= pivot) left++;
+                    while (left <= right && nums[right] >= pivot) right--;
+                    if (left <= right) swap(nums[left], nums[right]);
+                }
+                swap(nums[start], nums[right]);
+
+                /* Recursion */
+                quickSort(start, right - 1);
+                quickSort(right + 1, end);
             }
-            quickSort(start, right);
-            quickSort(left, end);
         };
         quickSort(0, nums.size() - 1);
         return nums;
