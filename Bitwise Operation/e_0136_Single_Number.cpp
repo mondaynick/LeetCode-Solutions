13d81df79e338d396f80c63a577819dcad3e05aa
MondayNick <51749165+mondaynick@users.noreply.github.com>
2024-07-16 09:28:29 +0800

    Update e_0136_Single_Number.cpp

--- Bitwise Operation/e_0136_Single_Number.cpp
+++ Bitwise Operation/e_0136_Single_Number.cpp
@@ -7,13 +7,8 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        if (nums.size() == 1) {
-            return nums[0];
-        }
-        int res = nums[0];
-        for (int i = 1 ; i < nums.size() ; i++) {
-            res = res ^ nums[i];
-        }
-        return res;
+        for (int i = 1 ; i < nums.size() ; i++)
+            nums[i] ^= nums[i - 1];
+        return nums[nums.size() - 1];
     }
 };
