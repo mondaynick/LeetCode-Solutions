42cf19e333583a53ad662a211b013341f753a069
MondayNick <51749165+mondaynick@users.noreply.github.com>
2024-07-17 09:58:33 +0800

    Update m_0752_Open_the_Lock.cpp

--- Breadth First Search/m_0752_Open_the_Lock.cpp
+++ Breadth First Search/m_0752_Open_the_Lock.cpp
@@ -8,30 +8,36 @@ class Solution {
 public:
     int openLock(vector<string>& deadends, string target) {
         if (target == "0000") return 0;
-        unordered_set<string> visited(deadends.begin(), deadends.end());
+        unordered_set<string> visited (deadends.begin(), deadends.end());
         if (visited.count("0000")) return -1;
 
         queue<string> q;
         q.push("0000");
+        int step = 0;
+        while (!q.empty()) {
+            step++;
+            for (int i = q.size() ; i > 0 ; i--) {
+                string temp = q.front();
+                q.pop();
+                for (int j = 0 ; j < 4 ; j++) {
+                    string prev = temp;
+                    prev[j] -= (prev[j] == '0') ? -9 : 1;
+                    if (prev == target) return step;
+                    if (!visited.count(prev)) {
+                        q.push(prev);
+                        visited.insert(prev);
+                    }
 
-        string curr, prev, next;
-        for (int size = 1, level = 1 ; !q.empty() ; size = q.size(), level++) {
-            while (size--) {
-                curr = prev = next = q.front(); q.pop();
-                for (int i = 0 ; i < 4 ; i++) {
-                    next[i] += (next[i] == '9' ? -9 : 1);
-                    if (next == target) return level;
-                    if (!visited.count(next)) q.push(next), visited.insert(next);
-
-                    prev[i] -= (prev[i] == '0' ? -9 : 1);
-                    if (prev == target) return level;
-                    if (!visited.count(prev)) q.push(prev), visited.insert(prev);
-
-                    prev = next = curr;
+                    string next = temp;
+                    next[j] += (next[j] == '9') ? -9 : 1;
+                    if (next == target) return step;
+                    if (!visited.count(next)) {
+                        q.push(next);
+                        visited.insert(next);
+                    }
                 }
             }
         }
-
         return -1;
     }
 };
