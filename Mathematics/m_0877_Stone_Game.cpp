0155dad31240971768ccc89fe9f4e05dfce8893c
MondayNick <51749165+mondaynick@users.noreply.github.com>
2024-09-05 09:24:12 +0800

    Create m_0877_Stone_Game.cpp

---
+++ Mathematics/m_0877_Stone_Game.cpp
@@ -0,0 +1,12 @@
+/*
+  有偶數堆石頭且所有石頭的重量和為奇數，Alice和Bob每次只能從這堆石頭的最前面或最後面拿取石頭，當石頭被拿完後，擁有石頭重量最重者即獲勝
+  由Alice先拿，若Alice獲勝則回傳true，若Bob獲勝則回傳false
+
+  因為所有石頭重量和為奇數，故可分成兩堆石頭，一堆比較重一堆比較輕，Alice先拿必獲勝
+ */
+
+class Solution {
+public:
+    bool stoneGame(vector<int>& piles) {
+        return true;
+    }
