a77a8c700bfadcd7eb594533d2ba004f866b217b
MondayNick <51749165+mondaynick@users.noreply.github.com>
2025-05-07 12:00:21 +0800

    Create m_3341_Find_Minimum_Time_to_Reach_Last_Room_I.cpp

---
+++ Shortest Path/m_3341_Find_Minimum_Time_to_Reach_Last_Room_I.cpp
@@ -0,0 +1,50 @@
+/*
+  有一個地牢，其中有 n x m 個房間排成網格狀，給定一個 n x m 的二維陣列 moveTime ，
+  其中 moveTime[i][j] 代表從起點 (0, 0) 開始，你可以移動到該房間的最小時間，假設你
+  從起點 (0, 0) 且時間 t = 0 開始，房間和房間之間的移動需花費 1 秒，且你可以按照上、
+  下、左、右去遍歷各個房間，回傳到終點 (n - 1, m - 1) 的最小時間為何
+
+  此為一個 min-max problem ，利用 Dijkstra's algorithm ：
+    定義 distance[i][j] 為從起點 (0, 0) 到 (i, j) 所需的最小時間，
+    起初 distance[0][0] 先設定為 0 ，並將其他 distance[i][j] 設為無限大，
+    利用 priority queue 每次取得最小距離的房間進行鬆弛操作：
+      (1) 若此房間為終點，返回其紀錄的時間
+      (2) 若此房間相鄰的時間小於目前的房間，代表往下走還是取決於目前的房間，沒有意義
+      (3) 否則，更新到此相鄰房間的最小時間
+    即可得到從起點 (0, 0) 到終點 (n - 1, m - 1) 的最小時間
+ */
+
+class Solution {
+public:
+    int minTimeToReach(vector<vector<int>>& moveTime) { 
+        int rowlen = moveTime.size();
+        int collen = moveTime[0].size();
+        vector<vector<int>> distance(rowlen, vector<int>(collen, INT_MAX));
+        distance[0][0] = 0;
+        priority_queue<array<int, 3>, vector<array<int, 3>>, decltype([](auto &lhs, auto &rhs) {
+            return lhs[0] > rhs[0];
+        })> pq;
+        pq.push({0, 0, 0});
+        array<int, 5> dir = {-1, 0, 1, 0, -1};
+        while (true) {
+            auto [dist, row, col] = pq.top();
+            pq.pop();
+            if (row == rowlen - 1 && col == collen - 1) {
+                return dist;
+            } else if (distance[row][col] < dist) {
+                continue;
+            }
+            for (int i = 0 ; i < 4 ; ++i) {
+                int drow = row + dir[i];
+                int dcol = col + dir[i + 1];
+                if (drow >= 0 && drow < rowlen && dcol >= 0 && dcol < collen) {
+                    int time = max(moveTime[drow][dcol], distance[row][col]) + 1;
+                    if (time < distance[drow][dcol]) {
+                        distance[drow][dcol] = time;
+                        pq.push({time, drow, dcol});
+                    }
+                }
+            }
+        }
+    }
+};
